f27cf6f4d6c276290eefaf3be560b28e742ae283
notfoundout <lexdekidd@icloud.com>
2026-07-10 00:07:22 -0400

    Revert "Add Phase 3 mechanization IR foundation (#151)"
    This reverts commit e19b1aec49a83bb202bdeef3ee9881908b90985b.

--- mechanization/far_mechanization/errors.py
+++
@@ -1,4 +0,0 @@
-"""Programmer-error exceptions for FAR mechanization internals."""
-
-class FARMechanizationError(RuntimeError):
-    """Raised only for impossible internal states or programmer errors."""
