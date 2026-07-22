c378576a2978c84710a72ab83e2f576433f7a3e9
notfoundout <lexdekidd@icloud.com>
2026-07-22 17:37:08 -0400

    Extend scope challenge queue regression tests through SC-W5

--- tests/test_post_w9_internal_scope_challenge.py
+++ tests/test_post_w9_internal_scope_challenge.py
@@ -21,6 +21,8 @@ class PostW9InternalScopeChallengeTests(unittest.TestCase):
             self.assertEqual(queue['ordered_followups'],[271,272,273,274,275])
         if next_pr==272:
             self.assertEqual([x['target_pr'] for x in queue['completed_workstreams']],[270,271]); self.assertEqual(queue['ordered_followups'],[273,274,275]); self.assertEqual(queue['next_action']['workstream'],'SC-W3-CONTRACT-LADDER')
-        if next_pr>=273:
+        if next_pr==273:
             self.assertEqual([x['target_pr'] for x in queue['completed_workstreams']],[270,271,272]); self.assertEqual(queue['ordered_followups'],[274,275]); self.assertEqual(queue['next_action']['workstream'],'SC-W4-REPRESENTATION-ESCAPE')
+        if next_pr>=274:
+            self.assertEqual([x['target_pr'] for x in queue['completed_workstreams']],[270,271,272,273]); self.assertEqual(queue['ordered_followups'],[275]); self.assertEqual(queue['next_action']['workstream'],'SC-W5-HELD-OUT-CONTEXTS')
 if __name__=='__main__':unittest.main()
