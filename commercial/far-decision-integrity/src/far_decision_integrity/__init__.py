e08ad975f0b10799b9d2c08ed6670a772ebbeced
notfoundout <lexdekidd@icloud.com>
2026-07-23 14:08:58 -0400

    Export refund authorization API

--- commercial/far-decision-integrity/src/far_decision_integrity/__init__.py
+++ commercial/far-decision-integrity/src/far_decision_integrity/__init__.py
@@ -1,4 +1,5 @@
 from .adjudicate import Adjudication, Finding, adjudicate
+from .authorize import AuthorizationDecision, RuntimeDisposition, authorize
 from .io import load_package
 from .model import (
     SCHEMA_VERSION,
@@ -8,18 +9,25 @@ from .model import (
     IntegrityStatus,
     PackageValidationError,
 )
+from .refund import REFUND_POLICY_VERSION, RefundRequest, build_refund_package
 from .report import report_payload, write_report
 
 __all__ = [
     "SCHEMA_VERSION",
     "Adjudication",
+    "AuthorizationDecision",
     "DecisionNode",
     "DecisionPackage",
     "Dependency",
     "Finding",
     "IntegrityStatus",
     "PackageValidationError",
+    "REFUND_POLICY_VERSION",
+    "RefundRequest",
+    "RuntimeDisposition",
     "adjudicate",
+    "authorize",
+    "build_refund_package",
     "load_package",
     "report_payload",
     "write_report",
